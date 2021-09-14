588f543922652f2989988ce8cbda8ca2503909a7
Sarita Nair <sn333546@gmail.com>
2021-09-13 20:02:58 -0500

    test changes

--- cypress/integration/sortcompanies.spec.js
+++ cypress/integration/sortcompanies.spec.js
@@ -10,31 +10,31 @@ describe("example to-do app", () => {
   });
 
   it("should allow case insensitive search", function () {
-    cy.get("input").type("AMA");
-    cy.get("#myTable").contains("Amazon").should("exist");
+    cy.get("input").type("nasa");
+    cy.get("#myTable").contains("NASA").should("exist");
   });
 
-  //   it("Verify if respective company name is displayed on entering white space before 1st 2 alphabets as the input", function () {
-  //     // default
-  //     cy.get("input").type(" ve");
-  //     cy.get("#myTable").contains("Verizon").should("exist");
-  //   });
+  it("Verify if respective company name is displayed on entering white space before 1st 2 alphabets as the input", function () {
+    // default
+    cy.get("input").type(" ve");
+    cy.get("#myTable").contains("Verizon").should("exist");
+  });
 
-  //   it("Verify if respective company name is displayed on entering white space after 1st 2 alphabets as the input", function () {
-  //     // default
-  //     cy.get("input").type("ve ");
-  //     cy.get("#myTable").contains("Verizon").should("exist");
-  //   });
+  it("Verify if respective company name is displayed on entering white space after 1st 2 alphabets as the input", function () {
+    // default
+    cy.get("input").type("ve ");
+    cy.get("#myTable").contains("Verizon").should("exist");
+  });
 
-  //   it("Verify if no company name is displayed on entering 3 random numbers as the input", function () {
-  //     // default
-  //     cy.get("input").type("123");
-  //     cy.get("#myTable").contains("123").should("not.exist");
-  //   });
+  it("Verify if no company name is displayed on entering 3 random numbers as the input", function () {
+    // default
+    cy.get("input").type("123");
+    cy.get("#myTable").contains("123").should("not.exist");
+  });
 
-  //   it("Verify if no company name is displayed on entering special characters as the input", function () {
-  //     // default
-  //     cy.get("input").type("F@");
-  //     cy.get("#myTable").contains("123").should("not.exist");
-  //   });
+  it("Verify if no company name is displayed on entering special characters as the input", function () {
+    // default
+    cy.get("input").type("F@");
+    cy.get("#myTable").contains("123").should("not.exist");
+  });
 });
